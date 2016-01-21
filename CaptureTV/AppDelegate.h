1cf208cd316b16def12a12ed57cf057445462036
李喆 <lizhe1@wisetv.com.cn>
2016-01-21 10:30:47 +0800

    Initial Commit

---
+++ CaptureTV/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CaptureTV
+//
+//  Created by mac on 16/1/21.
+//  Copyright © 2016年 TJGD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CaptureTV/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CaptureTV
+//
+//  Created by mac on 16/1/21.
+//  Copyright © 2016年 TJGD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
